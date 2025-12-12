0c82b2eaf6fb5bbb5286864355576c851d5bfde8
juliankeites <79912113+juliankeites@users.noreply.github.com>
2025-12-12 07:41:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ def pnl_from_long_payoff(long_payoff, premium_per_unit_now, qty=1.0, is_long=Tru
 # ---------- Streamlit app ----------
 
 def main():
-    st.set_page_config(page_title="Vanilla European Oil Option Payoff & Greeks", layout="wide")
-    st.title("Oil Option Payoff, P&L and Greeks - by J Keites linkedin.com/in/jkeites")
+    st.set_page_config(page_title="Vanilla European Oil Option", layout="wide")
+    st.title("European Oil Option Payoff, P&L and Greeks - by J Keites linkedin.com/in/jkeites")
 
     # Sidebar inputs
     st.sidebar.header("Vanilla European Option inputs")
